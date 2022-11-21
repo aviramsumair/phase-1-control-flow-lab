2dcab33b745e9c26d71c46265b234d8c2e12167a
Avinash Ramsumair <avi@pure235games.com>
2022-11-20 23:24:16 -0500

    commit tree c15c6e671bee043081058f6aad64c58973646d63

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  if (distance > 2500){
+    return 'No can do.';
+  }
+  else if (distance > 2000){
+    return 'I will gladly take your thirty bucks.';
+  }
+  else if (distance > 400){
+    return `That will be twenty bucks.`;
+  }
+  else if (distance <= 400){
+    return `This one is on me!`;
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(cityName){
+  return cityName === "NYC" ? `Ok, sounds good.` : `No go.`;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tipAmoun){
+  switch(tipAmoun){
+    case 'generous' : return `Thank you so much.`;
+    case `not as generous` : return 'Thank you.';
+    default : return `Bye.`;
+  }
 }
